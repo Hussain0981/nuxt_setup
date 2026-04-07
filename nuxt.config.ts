afee7ebad855a15f44a6778b330861628fb6d57f
hussain ullah <enghussainullah@gmail.com>
2026-04-07 10:52:00 +0500

    add: nuxt image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/eslint',
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/image'],
   devtools: { enabled: true },
   css: [
     '~/assets/css/main.css',
