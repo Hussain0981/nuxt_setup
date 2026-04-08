da6aa7edec202b842dadca40e9f652c07ca4fd1f
hussain ullah <enghussainullah@gmail.com>
2026-04-08 15:37:08 +0500

    add: basic setup

---
+++ app/components/TheBackend.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Backend</h1>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+
+</style>
---
+++ app/components/TheFrontend.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+const basic = [
+  {
+    id: 1,
+    title: 'Html',
+    logo: '',
+  },
+  {
+    id: 2,
+    title: 'CSS',
+    logo: '',
+  },
+  {
+    id: 3,
+    title: 'Javascript',
+    logo: '',
+  },
+]
+const styling = [
+  { id: 1, title: 'TailwindCSS', logo: '' },
+  { id: 2, title: 'UnoCSS', logo: '' },
+]
+const animations = [
+  { id: 1, title: 'GSAP', logo: '' },
+  { id: 2, title: 'Three.js', logo: '' },
+]
+const languages = [
+  { id: 1, title: 'Javascript', logo: '' },
+  { id: 2, title: 'Typescript', logo: '' },
+]
+const advance = [
+  { id: 1, title: 'Vue', logo: '' },
+  { id: 2, title: 'Nuxt', logo: '' },
+]
+</script>
+
+<template>
+  <div>
+    <h1>Frontend</h1>
+  </div>
+</template>
+
+<style lang="postcss" scoped></style>
